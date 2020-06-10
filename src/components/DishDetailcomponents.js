5ea1d312db221726ed68092e0ff3379b54707141
HIMRAJ GOGOI <galacticmacawesomeville@gmail.com>
2020-06-10 16:08:44 +0530

    assignment1 and presentational and container components

---
+++ src/components/DishDetailcomponents.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+
+class DishDetails extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    renderDish(dish) {
+            return ( <
+                Card >
+                <
+                CardImg width = "100%"
+                src = { dish.image }
+                alt = { dish.name } >
+
+                <
+                /CardImg> <
+                CardBody >
+                <
+                CardTitle > { dish.name } < /CardTitle> <
+                CardText > { dish.description } < /CardText> < /
+                CardBody > <
+                /Card> );
+
+            }
+            renderComments(comments) {
+                return ( < div > < h4 > Comments < /h4> <
+                    ul className = "list-unstyled" >
+                    <
+                    li className = "mt-4" > { comments[0].comment } < /li>  <
+                    li className = "mt-4" > --{ comments[0].author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments[0].date))) } < /li>  <
+                    li className = "mt-4" > { comments[1].comment } < /li>  <
+                    li className = "mt-4" > --{ comments[1].author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments[1].date))) } < /li>  <
+                    li className = "mt-4" > { comments[2].comment } < /li>  <
+                    li className = "mt-4" > --{ comments[2].author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments[2].date))) } <
+                    /li>  <
+                    li className = "mt-4" > { comments[3].comment } < /li>  <
+                    li className = "mt-4" > --{ comments[3].author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments[3].date))) } < /li>  <
+                    li className = "mt-4" > { comments[4].comment } < /li>  <
+                    li className = "mt-4" > --{ comments[4].author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments[4].date))) } < /li> < /
+                    ul > < /div >
+                );
+            }
+            render() {
+                if (this.props.dish != null) {
+                    return ( < div className = "container" >
+                        <
+                        div className = "row" >
+                        <
+                        div className = "col-12 col-md-5 m-1" > { this.renderDish(this.props.dish) } <
+                        /div> <
+                        div className = "col-12 col-md-5 m-1" > { this.renderComments(this.props.dish.comments) } <
+                        /div>
+
+                        <
+                        /div> < /
+                        div >
+                    );
+                } else {
+                    return ( < div > < /div>);
+                    }
+
+
+
+
+                }
+            }
+
+
+            export default DishDetails;
--- src/components/menucomponents.js
+++ src/components/menucomponents.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle } from 'reactstrap';
+
 class Menu extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-    }
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return ( <
-                Card >
-                <
-                CardImg width = "100%"
-                src = { dish.image }
-                alt = { dish.name }
-                />  <
-                CardBody >
-                <
-                CardTitle > { dish.name } < /CardTitle> <
-                CardText > { dish.description } < /CardText> < /
-                CardBody > <
-                /Card>
-            )
-        } else {
-            return ( <
-                div > < /div>
-            )
-        }
-    }
+
+
+
 
     render() {
         const menu = this.props.dishes.map((dish) => {
@@ -42,7 +17,8 @@ class Menu extends Component {
                 className = "col-12 col-md-5 m-1" >
                 <
                 Card onClick = {
-                    () => this.onDishSelect(dish)
+                    () => this.props.onClick(dish.id)
+
                 } >
 
                 <
@@ -58,20 +34,18 @@ class Menu extends Component {
                 div >
             );
         });
-
         return ( <
             div className = "container" >
             <
             div className = "row" >
 
             { menu } <
-            /div > <
-            div className = "row" > {
-                this.renderDish(this.state.selectedDish)
-            } <
-            /div> < /
-            div >
+            /div> <
+            /div>
         );
+
+
+
     }
 }
 
