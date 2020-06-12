859c133ee87f396cbda4de34130ac4c127f32909
HIMRAJ GOGOI <galacticmacawesomeville@gmail.com>
2020-06-12 15:32:07 +0530

    single page applications p1

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Header from './headercomponent';
 import Footer from './footercomponent';
 import Menu from './menucomponents';
+import Contact from './contactcomponent';
 import Home from './homecomponent';
 import DishDetails from './DishDetailcomponents';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,6 +16,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
             selectedDish: null
         };
     }
@@ -19,7 +26,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return ( <
-                Home / >
+                Home dish = { this.state.dishes.filter((dish) => dish.featured)[0] }
+                leader = { this.state.leaders.filter((leader) => leader.featured)[0] }
+                promotion = { this.state.promotions.filter((promo) => promo.featured)[0] }
+                / >
             );
         }
         return ( <
@@ -31,7 +41,10 @@ class Main extends Component {
             <
             Route path = "/home"
             component = { HomePage }
-            /> <
+            />  <
+            Route exact path = "/contactus"
+            component = { Contact }
+            /><
             Route exact path = "/menu"
             component = {
                 () => < Menu dishes = { this.state.dishes }
